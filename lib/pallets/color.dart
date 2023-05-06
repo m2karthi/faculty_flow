b7dded6b9a7f6c3a053b6d2a6f00acd4b307fae7
m2karthi <68001099+m2karthi@users.noreply.github.com>
2023-05-06 09:33:11 +0530

    file arrangement

---
+++ lib/pallets/color.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static const Color bodyColor = Color(0xFFFFFFFF);
+  static const Color buttonColor = Color(0xFF7059DE);
+  static const Color titleColor = Color(0x000000);
+  static const Color iconColor = Color(0x000000);
+  static const Color placeholderColor = Color(0xFF757575);
+  static const Color secondaryTextColor = Color(0xFF1573FE);
+  static const Color profileColor = Color(0xFFB3DCF1);
+  // static const Color comingSoonColor = Color(0xFFCAD996);
+}
+
+
+
