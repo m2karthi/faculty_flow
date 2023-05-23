87091ebeeb7badc16476e2bbdb925c2bd7aa235a
m2karthi <68001099+m2karthi@users.noreply.github.com>
2023-05-24 00:21:14 +0530

    Login error solved

--- lib/widgets/onboarding/template.dart
+++ lib/widgets/onboarding/template.dart
@@ -136,7 +136,7 @@ class ScreenTemp extends StatelessWidget {
           curve: Curves.easeInOut,
           animationDuration: Duration(milliseconds: 350),
           // home: NavBar(),
-          home: LoginPage(),
+          home: NavBar(),
         ),
         // back: const Back(child: Icon(Icons.arrow_back)),
       ),
