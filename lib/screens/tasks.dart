ba9b6cf4f3785e580161bf081d125dfc10377373
m2karthi <68001099+m2karthi@users.noreply.github.com>
2023-05-24 21:43:33 +0530

    Update tasks.dart

--- lib/screens/tasks.dart
+++ lib/screens/tasks.dart
@@ -1,6 +1,13 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:faculty_flow/services/task.service.dart';
 import 'package:faculty_flow/widgets/taskcard.dart';
 import 'package:flutter/material.dart';
 import 'package:calendar_timeline/calendar_timeline.dart';
+import 'package:lottie/lottie.dart';
 
 class TasksPage extends StatefulWidget {
   const TasksPage({super.key});
@@ -10,21 +17,33 @@ class TasksPage extends StatefulWidget {
 }
 
 class _TasksPageState extends State<TasksPage> {
-  // late DateTime selectedDate;
-  // late DateTime firstDate;
-  // late DateTime lastDate;
-  var tasks = [
-    { 
-      "title": 'Complete Project Report'
+  TaskService _taskService = TaskService();
 
-    }
-  ];
-
-  // void initState() {
+  //    void initState() {
   //   super.initState();
-    var selectedDate = DateTime.now();
-    var firstDate = DateTime.now();
-    var lastDate = DateTime.now().add(const Duration(days: 365 * 4));
+  //   _fetchProfile();
+  // }
+
+  var selectedDate = DateTime.now();
+  var firstDate = DateTime.now();
+  var lastDate = DateTime.now().add(const Duration(days: 365 * 4));
+
+  List<Map<String, dynamic>> filterObjectsByDate(
+      objects, DateTime selectedDate) {
+    return objects.where((obj) {
+      var dat = obj['date'];
+      print("dat $dat ");
+      final dateTime = DateTime.fromMillisecondsSinceEpoch(
+          dat.microsecondsSinceEpoch ~/ 1000);
+      print("dateTime $dateTime ");
+      print("Selected Date $selectedDate");
+
+      return dateTime.year == selectedDate.year &&
+          dateTime.month == selectedDate.month &&
+          dateTime.day == selectedDate.day;
+    }).toList();
+  }
+
   // }
   @override
   Widget build(BuildContext context) {
@@ -32,60 +51,119 @@ class _TasksPageState extends State<TasksPage> {
         appBar: AppBar(
           title: Text('Tasks'),
         ),
-        body: Column(
-          children: [
-            SizedBox(
-              height: 10,
-            ),
-            CalendarTimeline(
-              initialDate: selectedDate,
-              firstDate: firstDate,
-              lastDate: lastDate,
-              onDateSelected:(date) => setState(() => selectedDate = date),
-              leftMargin: 20,
-              monthColor: Colors.blue.shade300,
-              dayColor: Colors.teal[200],
-              activeDayColor: Colors.white,
-              activeBackgroundDayColor: Color(0xFF7D88E7),
-              dotsColor: Colors.white,
-              // selectableDayPredicate: (date) => date.day != 23,
-              locale: 'en_ISO',
-            ),
-            SizedBox(
-              height: 10,
-            ),
-          Expanded(
-              child: ListView(
-                // padding: EdgeInsets.all(16.0),
-                children: [
-                  TaskCard(
-                    label: 'Urgent',
-                    title: 'Complete Project Report',
-                    startTime: '9:00 AM',
-                    endTime: '11:30 AM',
-                  ),
-                  TaskCard(
-                    label: 'High',
-                    title: 'Meeting with Team',
-                    startTime: '2:00 PM',
-                    endTime: '3:00 PM',
-                  ),
-                  TaskCard(
-                    label: 'Normal',
-                    title: 'Send Progress Update',
-                    startTime: '4:30 PM',
-                    endTime: '5:00 PM',
-                  ),
-                  TaskCard(
-                    label: 'Low',
-                    title: 'Organize Files',
-                    startTime: '6:00 PM',
-                    endTime: '7:00 PM',
-                  ),
-                ],
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(
+                height: 10,
               ),
-            ),
-          ],
+              CalendarTimeline(
+                initialDate: selectedDate,
+                firstDate: firstDate,
+                lastDate: lastDate,
+                onDateSelected: (date) => setState(() => selectedDate = date),
+                leftMargin: 20,
+                monthColor: Colors.blue.shade300,
+                dayColor: Colors.teal[200],
+                activeDayColor: Colors.white,
+                activeBackgroundDayColor: Color(0xFF7D88E7),
+                dotsColor: Colors.white,
+                // selectableDayPredicate: (date) => date.day != 23,
+                locale: 'en_ISO',
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              FutureBuilder(
+                  future: _taskService.getTodayTasks(),
+                  builder: (BuildContext context, AsyncSnapshot snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      // Display a loading spinner while waiting for data
+                      return const Center(child: CircularProgressIndicator());
+                    } else if (snapshot.hasError) {
+                      // Display an error message if the future throws an error
+                      return Text("Error: ${snapshot.error}");
+                    } else {
+                      print("SNapshot $snapshot");
+                      print("SNapshot data ${snapshot.data} ");
+                      // print("index SNapshot data  ${snapshot.data[0]} ");
+                      // var tasksData = snapshot.data;
+                      // print("Tasks data ${tasksData['title']}");
+
+                      // List<Map<String, dynamic>> reqtasks =
+                      //     filterObjectsByDate(snapshot.data, selectedDate);
+                      var reqtasks = [];
+                      for (var task in snapshot.data) {
+                        var date = task['date'];
+                        print("date from fire ${date}");
+                        var convertedDate = date.toDate();
+                        print("convertedDate from fire ${convertedDate}");
+
+                        var formatDate =
+                            DateFormat('yyyy-MM-dd').format(convertedDate);
+
+                        print("formatDate from fire ${formatDate}");
+                        print("Selected Date ${selectedDate}");
+
+                        var formatSelectedDate =
+                            DateFormat('yyyy-MM-dd').format(selectedDate);
+                        print(
+                            "formatSelectedDate from fire ${formatSelectedDate}");
+
+                        if (formatDate == formatSelectedDate) {
+                          reqtasks.add(task);
+                        }
+                      }
+
+                      print("reqtasks ${reqtasks}");
+
+                      if (reqtasks.length > 0) {
+                        return Column(
+                          children: [
+                            // Text("data")
+                            SizedBox(
+                              height: 700,
+                              width: double.infinity,
+                              child: Expanded(
+                                child: ListView.builder(
+                                    itemCount: reqtasks.length,
+                                    itemBuilder:
+                                        (BuildContext context, int index) {
+                                      var taskdata = reqtasks[index];
+                                      return TaskCard(
+                                          label: taskdata['tag'],
+                                          title: taskdata['title'],
+                                          startTime: taskdata['startTime'],
+                                          endTime: taskdata['endTime']);
+                                    }),
+                              ),
+                            ) 
+                          ],
+                        );
+                      } else {
+                        return Column(
+                          children: [
+                            Container(
+                              width: 350, // Set the desired width
+                              // height: 200, // Set the desired height
+                              child: Lottie.network(
+                                  'https://assets9.lottiefiles.com/private_files/lf30_lkquf6qz.json'), // Replace with your Lottie animation file path
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text("No Tasks Found! Enjoy by adding new Tasks",
+                                style: GoogleFonts.hindSiliguri(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize: 15))
+                          ],
+                        );
+                      }
+                    }
+                  })
+            ],
+          ),
         ));
   }
 }
